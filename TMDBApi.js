f92cae6c05ca0f7f731d069f6c2165443aa0cc76
Gurottesque <a.bedoya3@utp.edu.co>
2024-07-04 23:42:48 -0500

    Add files via upload

--- TMDBApi.js
+++ TMDBApi.js
@@ -4,12 +4,21 @@ import dotenv from 'dotenv';
 import movie from './endpoints/movies.js';
 import tv from './endpoints/tv.js';
 import people from './endpoints/people.js';
+import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 
 dotenv.config();
 
 const app = express();
 const port = 3000;
 const KEY = process.env.KEY;
+
+app.use("/api", express.static(path.join(__dirname, "public")));
 
 app.use('/api/movies', movie)
 
